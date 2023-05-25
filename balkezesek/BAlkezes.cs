f46ec9638a3c3e9a7eb1f22eb974f30618602ce3
FeketeClaw <FeketeClaw@REVISION-PC>
2023-05-25 12:52:18 +0000

    Ebéd után

--- balkezesek/BAlkezes.cs
+++ balkezesek/BAlkezes.cs
@@ -8,5 +8,25 @@ namespace balkezesek
 {
     internal class BAlkezes
     {
+        string nev;
+        string palyaralepes;
+        string utolsoPalya;
+        int suly;
+        int magassag;
+
+        public BAlkezes(string nev, string palyaralepes, string utolsoPalya, int suly, int magassag)
+        {
+            this.nev = nev;
+            this.palyaralepes = palyaralepes;
+            this.utolsoPalya = utolsoPalya;
+            this.suly = suly;
+            this.magassag = magassag;
+        }
+
+        public string Nev { get => nev; set => nev = value; }
+        public string Palyaralepes { get => palyaralepes; set => palyaralepes = value; }
+        public string UtolsoPalya { get => utolsoPalya; set => utolsoPalya = value; }
+        public int Suly { get => suly; set => suly = value; }
+        public int Magassag { get => magassag; set => magassag = value; }
     }
 }
--- balkezesek/Program.cs
+++ balkezesek/Program.cs
@@ -1,3 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using balkezesek;
+using System;
+using System.Text.Json.Nodes;
+using System.Xml;
 
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static List<BAlkezes> jatekosok = new List<BAlkezes>();
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("balkezesek.csv");
+            jatekosok.Skip(1);
+            while (!sr.EndOfStream)
+            {
+                string[] tomb = sr.ReadLine().Split(';');
+            }
+            sr.Close();
+
+            Console.WriteLine($"3. feladat: {jatekosok.Count()}");
+
+            Console.WriteLine(jatekosok[4]);
+
+            /*
+            Console.WriteLine("5. feladat");
+            Console.WriteLine("Kérek egy 1990 és 1999 közötti évszámot!: ");
+            int beirtAdat = Convert.ToInt32(Console.ReadLine());
+            int seged = 0;
+            do
+            {
+
+                Console.WriteLine("Hibás adat! Kérek egy 1990 és 1999 közötti ávszámot!: ");
+                seged = Convert.ToInt32(Console.ReadLine());
+                beirtAdat = seged;
+            } while (beirtAdat <1990 ||beirtAdat >1999);
+            */
+        }
+    }
+}
